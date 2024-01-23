e72d2cb77849523fd40a5f6df23722d77c716949
Viktor Filippov <jackal3166@gmail.com>
2024-01-23 02:14:12 +0200

    Add subject

--- components/Focus.jsx
+++ components/Focus.jsx
@@ -2,12 +2,19 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { TextInput } from 'react-native-paper';
 import { colors } from '../utils/colors';
+import { useState } from 'react';
 
 const Focus = () => {
+	const [subject, setSubject] = useState(null);
+
 	return (
 		<View style={styles.container}>
 			<View style={styles.inputContainer}>
-				<TextInput label='What would you like to focus on?' />
+				<TextInput
+					label='What would you like to focus on?'
+					value={subject}
+					onChangeText={(subject) => setSubject(subject)}
+				/>
 			</View>
 		</View>
 	);
